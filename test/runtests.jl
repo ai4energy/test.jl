1558ac83b4345129b2c6a3fef6f0881d9f5f023e
yjy <522432938@qq.com>
2022-03-18 10:46:12 +0800

    fixed my mistakes

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,5 @@ using Test
 @testset "test.jl" begin
     @test my_f(2,3) == 7
     @test my_f(2,1) == 5
-    @test my_f(2,2) == 5    # Write your tests here.
+    @test my_f(2,2) == 6    # Write your tests here.
 end
